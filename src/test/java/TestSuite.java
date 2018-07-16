fbf686155b1147b9e07ae21dcd02820b15c79a8c
aha <aha@scireum.de>
2018-07-16 11:50:39 +0200

    Adds a test-suite as expected by the build system.

---
+++ src/test/java/TestSuite.java
@@ -0,0 +1,17 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+import com.googlecode.junittoolbox.SuiteClasses;
+import com.googlecode.junittoolbox.WildcardPatternSuite;
+import org.junit.runner.RunWith;
+
+@RunWith(WildcardPatternSuite.class)
+@SuiteClasses({"**/*Test.class", "**/*Spec.class"})
+public class TestSuite {
+
+}
